08fabf33fe424838982bfef0ce42a1b0e4823337
gokultp08 <gokultp08@gmail.com>
2021-08-16 22:13:35 +0530

    version update

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const lodash = require('lodash')
 const function1 = (str) => {
-    return lodash.kebabCase(str) + '12345'
+    return lodash.kebabCase(str) + '   ' + lodash.camelCase(str)
 }
 module.exports = function1
